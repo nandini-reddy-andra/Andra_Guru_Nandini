3c015d9900adcaaa04a4ad31c45c84a2ccb78216
nandini-reddy-andra <123005511+nandini-reddy-andra@users.noreply.github.com>
2024-07-23 16:11:31 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,16 @@
+package nandini;
+
+public class Even {
+	public static void main(String args[])
+	{
+		int a=10;
+		if((a&1)==0)
+		{
+			System.out.println("Even");
+		}else
+		{
+			System.out.println("Odd");
+		}
+	}
+
+}
