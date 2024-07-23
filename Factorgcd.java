5b77ca420449d34927b387e69b12d310190d2d7a
nandini-reddy-andra <123005511+nandini-reddy-andra@users.noreply.github.com>
2024-07-23 16:11:54 +0530

    Add files via upload

---
+++ Factorgcd.java
@@ -0,0 +1,16 @@
+package nandini;
+
+public class Factorgcd {
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    public static void main(String[] args) {
+        int num1 = 24;
+        int num2 = 36;
+        System.out.println("GCD of " + num1 + " and " + num2 + " is: " + gcd(num1, num2));
+    }
+}
